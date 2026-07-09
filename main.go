d427cac0a55892195f622004230a090db8cfa112
Jam Carlos Ramirez Chaves <jamcarlo99@gmail.com>
2026-07-09 10:32:00 -0700

    TODO and Interfaces implementations

--- main.go
+++ main.go
@@ -7,7 +7,26 @@ import (
 	"strings"
 
 	"example.com/note/note"
+	"example.com/note/todo"
 )
+
+type saver interface {
+	SaveToJson() error
+}
+
+type outputable interface {
+	saver
+	Display()
+}
+
+// type displayer interface {
+// 	Display()
+// }
+
+// type outputable interface {
+// 	SaveToJson() error
+// 	Display()
+// }
 
 func main() {
 
@@ -20,17 +39,55 @@ func main() {
 		return
 	}
 
-	userNote.Display()
-
-	err = userNote.SaveToJson()
-
+	todoText := getTodoData()
+	todo, err := todo.New(todoText)
 	if err != nil {
-		fmt.Println("Saving the note Failed")
+		fmt.Println(err)
 		return
 	}
 
-	fmt.Println("Note has been save on JSON File")
+	err = outputData(userNote)
+	if err != nil {
+		return
+	}
 
+	err = outputData(todo)
+	if err != nil {
+		return
+	}
+
+}
+
+// These two Value1 and Value 2 are basically the same with eachother "interface{}" == any
+func printSomething(value interface{}, value2 any) {
+	switch value.(type) {
+	case int:
+		fmt.Println("Integer: ", value)
+	case float64:
+		fmt.Println("Float: ", value)
+	case string:
+		fmt.Println("Float: ", value)
+	default:
+		fmt.Println("Any Value: ", value)
+
+	}
+	fmt.Println(value)
+}
+
+func saveDataToJson(data saver) error {
+	err := data.SaveToJson()
+
+	if err != nil {
+		fmt.Println("Saving Data the note Failed")
+		return err
+	}
+	fmt.Println("DATA has been save on JSON File")
+	return nil
+}
+
+func outputData(data outputable) error {
+	data.Display()
+	return saveDataToJson(data)
 }
 
 func getUserInput(promt string) string {
@@ -49,6 +106,10 @@ func getUserInput(promt string) string {
 	value = strings.TrimSuffix(value, "\r")
 
 	return value
+}
+
+func getTodoData() string {
+	return getUserInput("TODO text: ")
 }
 
 func getNoteData() (string, string) {
